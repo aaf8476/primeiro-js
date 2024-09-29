bb3d0875790f1c22faa274a6c2cfa976f6d38275
adriana_ferreira <adriana.almeidinha@gmail.com>
2024-09-29 09:43:10 -0300

    exercicio para aula de js - conexão com arquivo js

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hello world!");
+let num1 = 15;
+let num2 = 27;
+console.log(num1+num2)
+
+const n = "Conexão feita com sucesso!";
+function imprimir (n){
+    return (n);
+}
+console.log(n)
+console.log(n)
+console.log(n)
+
+
+const n1 = "Conexão feita com sucesso! Parabéns!";
+
+function imprimir(n1) {
+    for (let i = 0; i < 3; i++) {
+        console.log(n1);
+    }
+}
+
+imprimir(n1)
